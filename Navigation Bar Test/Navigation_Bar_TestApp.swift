26ddd6cb78cbc885c7eed8ed4c2fba3d38e0a824
Millan Bhandari <millan.ib@icloud.com>
2022-07-29 10:26:41 -0500

    Initial Commit

---
+++ Navigation Bar Test/Navigation_Bar_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_Bar_TestApp.swift
+//  Navigation Bar Test
+//
+//  Created by Millan Bhandari on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_Bar_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
